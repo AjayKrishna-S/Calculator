614f5b739e1b4a00aeccf71cb7a32d9773d06429
Ajay Krishna <152839330+AjayKrishna-S@users.noreply.github.com>
2024-02-19 10:21:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let calculation = localStorage.getItem('calculation') || '';
+
+    function updateCalculation(number){
+            
+      calculation += number ;
+      document.querySelector('.js-cal-display').innerHTML = calculation;
+      localStorage.setItem('calculation',calculation);
+ 
+    }
+
+    function loadPage(){
+      document.querySelector('.js-cal-display').innerHTML = calculation;
+    }
+
+    function clickSound(){
+      var snd = new Audio('./Audio/button-sound.mp3');
+      snd.play();
+    }
